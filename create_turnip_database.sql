61739e720c4e797c0e122f356c4c6fee50a12866
Derek Yung-Sheng Lin <dyl14@sfu.ca>
2020-04-27 23:41:15 -0700

    rename timestamp_observed to datetime_observed

--- create_turnip_database.sql
+++ create_turnip_database.sql
@@ -9,14 +9,14 @@ CREATE TABLE buy_data (
 	user_id VARCHAR(16) NOT NULL,
     island_id VARCHAR(32) NOT NULL,
     price SMALLINT UNSIGNED,
-    timestamp_observed timestamp,
-    PRIMARY KEY (user_id, island_id, timestamp_observed)
+    datetime_observed DATETIME,
+    PRIMARY KEY (user_id, island_id, datetime_observed)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE sell_data (
 	user_id VARCHAR(16) NOT NULL,
     island_id VARCHAR(32) NOT NULL,
     price SMALLINT UNSIGNED,
-    timestamp_observed timestamp,
-    PRIMARY KEY (user_id, island_id, timestamp_observed)
+    datetime_observed DATETIME,
+    PRIMARY KEY (user_id, island_id, datetime_observed)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
